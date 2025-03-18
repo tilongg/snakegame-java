07bf73e5179bffd283cdbb7856f44dee312f3ba0
user <@tilongg>
2025-03-18 11:04:40 +0700

    hello

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,6 @@
 public class Test {
     public static void main(String[] args) {
         System.out.print("Hellow");
-        System.out.print("dfdsf")
+        System.out.print("dfdsf");
     }
 }
